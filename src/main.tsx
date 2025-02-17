c3f73d3ff12f524c6ea3ccbcaf99219a1d05249a
shopeyinwale <shopeyinwale@gmail.com>
2025-02-17 11:57:02 +0000

    reservation date update

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
