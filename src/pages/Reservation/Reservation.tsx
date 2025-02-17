d60cafaad770c8b55caea2eb08eaa56b35df866a
shopeyinwale <shopeyinwale@gmail.com>
2025-02-17 11:19:44 +0000

    reservation page

--- src/pages/Reservation/Reservation.tsx
+++ src/pages/Reservation/Reservation.tsx
@@ -1,5 +1,26 @@
+import { Grid, Space, Text, TextInput } from "@mantine/core";
+import { DateTimePicker } from "@mantine/dates";
+import { useState } from "react";
+
 const Reservation = () => {
-  return <div className="container">Reservation</div>;
+  const [value, setValue] = useState<Date | null>(null);
+
+  return (
+    <div className="container">
+      <Grid gutter="md">
+        <Grid.Col span={6}>
+          <Text size="lg">Details</Text>
+          <Text size="md">Adult 1</Text>
+          <TextInput label="First Name" />
+          <Space h="md" />
+          <TextInput label="Last Name" />
+          <Space h="md" />
+          <DateTimePicker value={value} onChange={setValue} />
+        </Grid.Col>
+        <Grid.Col span={6}>2</Grid.Col>
+      </Grid>
+    </div>
+  );
 };
 
 export default Reservation;
